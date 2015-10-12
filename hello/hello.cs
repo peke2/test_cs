857ae49522be46ac2ff69f1c636f72fd18e9b4a6
peke2 <abem@dc4.so-net.ne.jp>
2015-10-12 19:26:50 +0900

    C#用の最初のテストコードを作成。 コンパイルはマイクロソフトのcsc.exeを使用。

---
+++ hello/hello.cs
@@ -0,0 +1,17 @@
+//	最初のサンプル
+//	コンパイラは以下にあるcsc.exeを使用
+//
+//	C:\Windows\Microsoft.NET\Framework64\v4.0.30319
+//
+//	上記ディレクトリに予めパスを通しておくこと
+
+//	コンパイル方法
+//	csc hello.cs
+
+class Hello
+{
+	public static void Main()
+	{
+		System.Console.WriteLine("Hello");
+	}
+}
