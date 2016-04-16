d8a5693f9232ce1efe94ef08326992608afb9d07
abe <abem@dc4.so-net.ne.jp>
2016-04-16 10:14:55 +0900

    クラスのリストでFindした時の確認を追加。 対象のものが見つからない場合は null が返ってくることを確認できた。

--- list/find.cs
+++ list/find.cs
@@ -24,7 +24,23 @@ class ListFindTest
 		}
 	}
 
+	class ParamClass
+	{
+		public string		name;
+		public int			val;
+		public List<int>	list;
 
+		public ParamClass(string name, int val)
+		{
+			this.name = name;
+			this.val = val;
+			this.list = new List<int>();
+			this.list.Add(7);
+			this.list.Add(11);
+		}
+	}
+
+	//	数値リストでのFind挙動確認
 	public void findFromInteger()
 	{
 		List<int> list = new List<int>();
@@ -50,6 +66,7 @@ class ListFindTest
 	}
 
 
+	//	構造体リストでのFind挙動確認
 	public void findFromStruct()
 	{
 		List<PARAM> list = new List<PARAM>();
@@ -77,11 +94,44 @@ class ListFindTest
 		//Console.WriteLine("list count={0}", p.list.Count);	//	null参照で例外発生
 	}
 
+	//	クラスリストでのFind挙動確認
+	public void findFromClass()
+	{
+		List<ParamClass> list = new List<ParamClass>();
+
+		list.Add(new ParamClass("abcde", 999));
+		list.Add(new ParamClass("hello", 12));
+		list.Add(new ParamClass("www", 23));
+
+		ParamClass	p;
+
+		Console.WriteLine("---- Find [hello] ----");
+		p = list.Find(param=> param.name == "hello");
+		Console.WriteLine("result={0}->{1}", p.name, p.val);
+		Console.WriteLine("list count={0}", p.list.Count);
+
+		//	見つからない場合は、型<T>の規定値が返る
+		//	class の場合は NULL が返る
+		//	UnityではSTRUCTでもNULLが返ってきたような気がするが…
+		//	まあ、見つからなかった場合の処理とかちゃんとしろよってところか
+		Console.WriteLine("---- Find [Bad] ----");
+		p = list.Find(param=> param.name == "Bad");
+		if( null == p )	Console.WriteLine("NULL!");
+		else			Console.WriteLine("result={0}->{1}", p.name, p.val);
+		//Console.WriteLine("list count={0}", p.list.Count);	//	null参照で例外発生
+	}
+
 
 	static void Main()
 	{
 		ListFindTest	test = new ListFindTest();
+		Console.WriteLine("----数値リスト----");
 		test.findFromInteger();
+		Console.WriteLine("");
+		Console.WriteLine("----構造体リスト----");
 		test.findFromStruct();
+		Console.WriteLine("");
+		Console.WriteLine("----クラスリスト----");
+		test.findFromClass();
 	}
 }
