ba84c571ef97f2d8ea25cf8b1ea52876df02ccec
abe <abem@dc4.so-net.ne.jp>
2016-05-10 11:59:21 +0900

    ListのAddRangeの確認であるコメントを追加。

--- list/merge.cs
+++ list/merge.cs
@@ -8,6 +8,7 @@ class Merge
 		List<int>	la = new List<int>{1,2,3};		//	A
 		List<int>	lb = new List<int>{9,11,13};	//	B
 
+		//	List::AddRange() がリストの単純な結合であることを確認する
 		//	リストを結合してBの後ろにAがそのまま並んでいることを確認
 		//	ソートなどはされていない状態
 		lb.AddRange(la);
