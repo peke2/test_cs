20ef1781883b9954650bff8bc6a3a028f31748da
peke2 <abem@dc4.so-net.ne.jp>
2016-12-28 01:18:41 +0900

    初回起動時のコンパイラーのパス指定についてのドキュメントを追加。 今のところ、Windows環境以外でのコンパイルは試していない。

--- compile.sh
+++ compile.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 # cygwin上で .NET FrameworkのC#コンパイラにパスを通しておくこと
+# 以下1行をユーザーの .bashrc に追加する
+#
 # PATH=/cygdrive/C/Windows/Microsoft.NET/Framework/v4.0.30319:$PATH
+#
+# ■追加先の使い分け
+# ログイン時に1回だけ実行したい場合 → .bash_profile に追加
+# シェルを起動するたびに実行したい場合 → .bashrc に追加
 
 csc $1 2>&1 | iconv -f SJIS -t UTF8
