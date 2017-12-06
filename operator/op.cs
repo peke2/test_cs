9bc3b88a25837a2672cf3af487080b1872c6ca83
Masaki Abe <abem@dc4.so-net.ne.jp>
2017-12-06 16:42:42 +0900

    C#でのoperator操作の確認を追加。

---
+++ operator/op.cs
@@ -0,0 +1,38 @@
+//	operator ìÆçÏämîF
+
+using System;
+
+class Test
+{
+	class TestOp
+	{
+		int _val;
+		public TestOp(int a)
+		{
+			_val = a;
+		}
+
+		public static TestOp operator * (TestOp a, TestOp b)
+		{
+			return new TestOp(a._val * b._val);
+		}
+
+		public override string ToString()
+		{
+			return _val.ToString();
+		}
+	}
+
+	static void Main()
+	{
+		TestOp op_a = new TestOp(5);
+		TestOp op_b = new TestOp(3);
+
+		Console.WriteLine(op_a * op_b);	//	15
+
+		TestOp op_c = new TestOp(7);
+		op_c *= op_b;					//	Ç±ÇÃå`Ç≈Ç‡Ç¢ÇØÇÈ
+
+		Console.WriteLine(op_c);		//	21
+	}
+}
