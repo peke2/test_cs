311aebad9b45b81f0292b6f098be068e8955d22a
peke2 <abem@dc4.so-net.ne.jp>
2018-03-18 22:50:38 +0900

    辞書のキーが無い場合の例外挙動を復習。

--- dictionary/get_value.cs
+++ dictionary/get_value.cs
@@ -20,8 +20,10 @@ class GetValue
 		{
 			Console.WriteLine("key{0}={1}", key,  dict[key]);
 		}
-		catch(KeyNotFoundException e)
+		catch(KeyNotFoundException e)	//	明示的にキーの有無を判断する場合
+		//catch(Exception e)	//	一応、これでも検出は可能だけど、他の例外の可能性もある
 		{
+			Console.WriteLine(e.Message);
 			Console.WriteLine("key{0}は存在しません", key);
 		}
 	}
