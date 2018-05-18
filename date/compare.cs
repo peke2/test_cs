d5d2e1b7d194dde8845bc5856f2b21851b99bd24
Masaki Abe <abem@dc4.so-net.ne.jp>
2018-05-18 13:39:01 +0900

    確認できるようデバッグ出力を追加。

--- date/compare.cs
+++ date/compare.cs
@@ -11,33 +11,69 @@ class Compare
 		DateTime dc = new DateTime(2018,1,2);
 		DateTime dd = new DateTime(2017,12,31);
 
+		//	タイマー開始時近辺での比較
+		DateTime st = new DateTime(1970,1,1);
+		Console.WriteLine("da="+da.ToString());
+		Console.WriteLine("st="+st.ToString());
+		if( DateTime.Compare(da, st) <= 0 )
+		{
+			Console.WriteLine("da <= st");
+		}
+		else
+		{
+			Console.WriteLine("da > st");
+		}
+		Console.WriteLine("");
 
-		//	引数の「,」を条件の記号(==、<、>)に置き換えればそのまま読める
+		/*
+		public static int Compare(
+			DateTime t1,
+			DateTime t2
+		)
+		0 より小さい値
+		t1 が t2 よりも前の日時です。
+
+		0
+		t1 は t2 と同じです。
+
+		0 より大きい値
+		t1 が t2 より後の日時です。
+		*/
+
+		//	0と比較した場合、引数の「,」を条件の記号(==、<、>)に置き換えればそのまま読める
+		Console.WriteLine("da="+da.ToString());
+		Console.WriteLine("db="+db.ToString());
 		if( DateTime.Compare(da, db) == 0 )
 		{
-			Console.WriteLine("等しい");
+			Console.WriteLine("da == db");
 		}
 		else
 		{
-			Console.WriteLine("等しくない");
+			Console.WriteLine("da != db");
 		}
+		Console.WriteLine("");
 
+		Console.WriteLine("da="+da.ToString());
+		Console.WriteLine("dc="+dc.ToString());
 		if( DateTime.Compare(da, dc) < 0 )
 		{
-			Console.WriteLine("大きい");
+			Console.WriteLine("da < dc");
 		}
 		else
 		{
-			Console.WriteLine("大きくない");
+			Console.WriteLine("da >= dc");
 		}
+		Console.WriteLine("");
 
+		Console.WriteLine("da="+da.ToString());
+		Console.WriteLine("dd="+dd.ToString());
 		if( DateTime.Compare(da, dd) > 0 )
 		{
-			Console.WriteLine("小さい");
+			Console.WriteLine("da > dd");
 		}
 		else
 		{
-			Console.WriteLine("小さくない");
+			Console.WriteLine("da <= dd");
 		}
 	}
 }
